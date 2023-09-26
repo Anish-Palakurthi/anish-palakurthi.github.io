4e3c6488d0f4fa1700bee6b01c84c0a4ab4f306e
Anish-Palakurthi <anish.palakurthi@utexas.edu>
2023-09-26 09:17:50 -0500

    changed linking

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://anish-palakurthi.github.io/',
+  homepage: 'https://anishpalakurthi.com',
   title: 'AP.',
 }
 
